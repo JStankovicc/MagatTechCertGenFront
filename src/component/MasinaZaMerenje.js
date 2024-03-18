e5f5425653ffccaeb90acf438dc607e3e77ce238
Jovan Stankovic <j.stankovic001@gmail.com>
2024-03-18 17:34:27 +0100

    Update

--- src/component/MasinaZaMerenje.js
+++ src/component/MasinaZaMerenje.js
@@ -1,9 +1,186 @@
 import React from 'react';
+import "../styles/MasinaZaMerenje.css";
 
 const MasinaZaMerenje = () => {
     return (
         <div>
             <h1>Masina za merenje zica i kablova</h1>
+            <form>
+                <label htmlFor="zapisnikBroj">Zapisnik Broj:</label>
+                <input type="text" id="zapisnikBroj" name="zapisnikBroj"/><br/>
+
+                <label htmlFor="vrstaKontrolisanja">Vrsta kontrolisanja/overavanja:</label>
+                <select id="vrstaKontrolisanja" name="vrstaKontrolisanja">
+                    <option value="opcija1">Opcija 1</option>
+                    <option value="opcija2">Opcija 2</option>
+                    <option value="opcija3">Opcija 3</option>
+                </select><br/>
+
+                <label htmlFor="podnosilacZahteva">Podnosilac zahteva:</label>
+                <input type={"text"} id={"podnosilacZahteva"} list={"kompanije"}/>
+                <datalist id={"kompanije"}>
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                </datalist>
+                <br/>
+
+                <label htmlFor="vlasnikKorisnik">Vlasnik/korisnik:</label>
+                <input type={"text"} id={"vlasnikKorisnik"} list={"kompanije"}/>
+
+
+                <label htmlFor="serijskiBroj">Serijski broj:</label>
+                <input type="text" id="serijskiBroj" name="serijskiBroj"/><br/>
+
+                <label htmlFor="identifikacioniBroj">Identifikacioni broj:</label>
+                <input type="text" id="identifikacioniBroj" name="identifikacioniBroj"/><br/>
+
+                <label htmlFor="proizvodjac">Proizvodjac:</label>
+                <input type={"text"} id={"proizvodjaci"} list={"kompanije"}/>
+                <datalist id={"proizvodjaci"}>
+                    <option>Opcija 1</option>
+                    <option>Opcija 2</option>
+                </datalist>
+                <br/>
+
+                <label htmlFor="oznakaModela">Oznaka tipa/modela:</label>
+                <input type="text" id="oznakaModela" name="oznakaModela"/><br/>
+
+                <label htmlFor="sluzbenaOznaka">Sluzbena oznaka tipa/broj izjave o usaglasenosti:</label>
+                <select id="sluzbenaOznaka" name="sluzbenaOznaka">
+                    <option value="predlog1">Predlog 1</option>
+                    <option value="predlog2">Predlog 2</option>
+                    <option value="predlog3">Predlog 3</option>
+                </select><br/>
+
+                <label htmlFor="merniOpseg">Merni opseg:</label>
+                <select id="merniOpseg" name="merniOpseg">
+                    <option value="opcija1">Opcija 1</option>
+                    <option value="opcija2">Opcija 2</option>
+                    <option value="opcija3">Opcija 3</option>
+                </select><br/>
+
+                <label htmlFor="najmanjiPodeljak">Najmanji podeljak:</label>
+                <select id="najmanjiPodeljak" name="najmanjiPodeljak">
+                    <option value="opcija1">Opcija 1</option>
+                    <option value="opcija2">Opcija 2</option>
+                    <option value="opcija3">Opcija 3</option>
+                </select><br/>
+
+                <label htmlFor="klasaTacnosti">Klasa tacnosti:</label>
+                <select id="klasaTacnosti" name="klasaTacnosti">
+                    <option value="opcija1">Opcija 1</option>
+                    <option value="opcija2">Opcija 2</option>
+                    <option value="opcija3">Opcija 3</option>
+                </select><br/>
+
+                <label className="container">
+                    <h2>Mašina je funkcionalno ispravna:</h2>
+                    <input type="radio" id="da" name="funkcionalnaIspravnost" value="da" />
+                    <label htmlFor="da">DA</label>
+                    <input type="radio" id="ne" name="funkcionalnaIspravnost" value="ne" />
+                    <label htmlFor="ne">NE</label>
+                </label>
+
+                <label htmlFor="napomena">Napomena:</label>
+                <textarea id="napomena" name="napomena" rows="4" cols="50"></textarea><br/>
+
+                <label>Prečnik točka mašine:</label><br/>
+                <div className="rezultatiContainer">
+                    <input type="text" value="1. merenje" readOnly />
+                    <input type="text" id="rezultat1" name="rezultat1" className="rezultatiInput"/>
+                </div>
+                <div className="rezultatiContainer">
+                    <input type="text" value="2. merenje" readOnly />
+                    <input type="text" id="rezultat2" name="rezultat2" className="rezultatiInput"/>
+                </div>
+                <div className="rezultatiContainer">
+                    <input type="text" value="3. merenje" readOnly />
+                    <input type="text" id="rezultat3" name="rezultat3" className="rezultatiInput"/>
+                </div>
+
+                <label className="container">
+                    <h2>Provera ispravnog vođenja mernog materijala:</h2>
+                    <input type="radio" id="ispunjava" name="ispunjava" value="ispunjava" />
+                    <label htmlFor="ispunjava">ISPUNJAVA</label>
+                    <input type="radio" id="neIspunjava" name="ispunjava" value="neIspunjava" />
+                    <label htmlFor="neIspunjava">NE ISPUNJAVA</label>
+                    <input type="radio" id="nijePrimenljivo" name="ispunjava" value="nijePrimenljivo" />
+                    <label htmlFor="neIspunjava">NIJE PRIMENLJIVO</label>
+                </label>
+
+                <label className="container">
+                    <h2>Provera ispravnsti pokaznog uređaja:</h2>
+                    <input type="radio" id="ispunjava1" name="ispunjava1" value="ispunjava" />
+                    <label htmlFor="ispunjava1">ISPUNJAVA</label>
+                    <input type="radio" id="neIspunjava1" name="ispunjava1" value="neIspunjava" />
+                    <label htmlFor="neIspunjava1">NE ISPUNJAVA</label>
+                </label>
+
+
+                <label>Provera ispravnosti uređaja za odmeravanje dužina:</label>
+                <div className="rezultatiContainer">
+                    <input type="text" value="Dužina uzorka" readOnly />
+                    <input type="text" id="rezultat11" name="rezultat11" className="rezultatiInput"/>
+                </div>
+                <div className="rezultatiContainer">
+                    <input type="text" value="Debljina uzorka" readOnly />
+                    <input type="text" id="rezultat12" name="rezultat12" className="rezultatiInput"/>
+                </div>
+                <div className="rezultatiContainer">
+                    <input type="text" value="Pokazivanje mašine" readOnly />
+                    <input type="text" id="rezultat13" name="rezultat13" className="rezultatiInput"/>
+                </div>
+                <div className="rezultatiContainer">
+                    <input type="text" value="Odstupanje od stvarne vrednosti dužine" readOnly />
+                    <input type="text" id="rezultat14" name="rezultat14" className="rezultatiInput"/>
+                </div>
+                <div className="rezultatiContainer">
+                    <input type="text" value="Relativna greška izmerene dužine" readOnly />
+                    <input type="text" id="rezultat15" name="rezultat15" className="rezultatiInput"/>
+                </div>
+                <div className="rezultatiContainer">
+                    <input type="text" value="NDG" readOnly />
+                    <input type="text" id="rezultat16" name="rezultat16" className="rezultatiInput"/>
+                </div>
+
+                <label htmlFor="merniLenjir">Serijski broj Mernog lenjira:</label>
+                <select id="merniLenjir" name="merniLenjir">
+                    <option value="opcija1">Opcija 1</option>
+                    <option value="opcija2">Opcija 2</option>
+                    <option value="opcija3">Opcija 3</option>
+                </select><br/>
+
+                <label htmlFor="mernaLupa">Serijski broj Pomičnog merila:</label>
+                <select id="mernaLupa" name="mernaLupa">
+                    <option value="opcija1">Opcija 1</option>
+                    <option value="opcija2">Opcija 2</option>
+                    <option value="opcija3">Opcija 3</option>
+                </select><br/>
+
+                <label htmlFor="skinutiZigovi1">Skinuti Zigovi:</label>
+                <input type="text" id="skinutiZigovi1" name="skinutiZigovi1"/>
+                <input type="text" id="skinutiZigovi2" name="skinutiZigovi2"/><br/>
+
+                <label htmlFor="stavljeniZigovi1">Stavljeni Zigovi:</label>
+                <input type="text" id="stavljeniZigovi1" name="stavljeniZigovi1"/>
+                <input type="text" id="stavljeniZigovi2" name="stavljeniZigovi2"/><br/>
+
+                <label className="container">
+                    <h2>Merna letva ispunjava propisane zahteve:</h2>
+                    <input type="radio" id="ispunjava" name="ispunjava" value="ispunjava" />
+                    <label htmlFor="ispunjava">DA</label>
+                    <input type="radio" id="neIspunjava" name="ispunjava" value="neIspunjava" />
+                    <label htmlFor="neIspunjava">NE</label>
+                </label>
+
+                <label htmlFor="komentar">Komentar:</label>
+                <textarea id="komentar" name="komentar" rows="4" cols="50"></textarea>
+
+                <label htmlFor="datum">Datum:</label>
+                <input type="date" id="datum" name="datum"/><br/>
+
+                <input type="submit" value="Potvrdi"/>
+            </form>
         </div>
     );
 };
--- src/component/MernaTraka.js
+++ src/component/MernaTraka.js
@@ -1,201 +1,206 @@
 import React from 'react';
+import "../styles/MernaTraka.css";
 
 const MernaTraka = () => {
     return (
         <div>
             <h2>Merna traka sa viskom</h2>
             <form>
-                <label htmlFor="zapisnikBroj">Zapisnik Broj:</label><br/>
-                <input type="text" id="zapisnikBroj" name="zapisnikBroj"/><br/><br/>
+                <label htmlFor="zapisnikBroj">Zapisnik Broj:</label>
+                <input type="text" id="zapisnikBroj" name="zapisnikBroj"/><br/>
 
-                <label htmlFor="vrstaKontrolisanja">Vrsta kontrolisanja/overavanja:</label><br/>
+                <label htmlFor="vrstaKontrolisanja">Vrsta kontrolisanja/overavanja:</label>
                 <select id="vrstaKontrolisanja" name="vrstaKontrolisanja">
                     <option value="opcija1">Opcija 1</option>
                     <option value="opcija2">Opcija 2</option>
                     <option value="opcija3">Opcija 3</option>
-                </select><br/><br/>
+                </select><br/>
 
-                <label htmlFor="podnosilacZahteva">Podnosilac zahteva:</label><br/>
+                <label htmlFor="podnosilacZahteva">Podnosilac zahteva:</label>
                 <input type={"text"} id={"podnosilacZahteva"} list={"kompanije"}/>
                 <datalist id={"kompanije"}>
                     <option>Option 1</option>
                     <option>Option 2</option>
                 </datalist>
-                <br/><br/>
+                <br/>
 
-                <label htmlFor="vlasnikKorisnik">Vlasnik/korisnik:</label><br/>
+                <label htmlFor="vlasnikKorisnik">Vlasnik/korisnik:</label>
                 <input type={"text"} id={"vlasnikKorisnik"} list={"kompanije"}/>
 
 
-                <label htmlFor="serijskiBroj">Serijski broj:</label><br/>
-                <input type="text" id="serijskiBroj" name="serijskiBroj"/><br/><br/>
+                <label htmlFor="serijskiBroj">Serijski broj:</label>
+                <input type="text" id="serijskiBroj" name="serijskiBroj"/><br/>
 
-                <label htmlFor="identifikacioniBroj">Identifikacioni broj:</label><br/>
-                <input type="text" id="identifikacioniBroj" name="identifikacioniBroj"/><br/><br/>
+                <label htmlFor="identifikacioniBroj">Identifikacioni broj:</label>
+                <input type="text" id="identifikacioniBroj" name="identifikacioniBroj"/><br/>
 
-                <label htmlFor="proizvodjac">Proizvodjac:</label><br/>
+                <label htmlFor="proizvodjac">Proizvodjac:</label>
                 <input type={"text"} id={"proizvodjaci"} list={"kompanije"}/>
                 <datalist id={"proizvodjaci"}>
                     <option>Opcija 1</option>
                     <option>Opcija 2</option>
                 </datalist>
-                <br/><br/>
+                <br/>
 
-                <label htmlFor="oznakaModela">Oznaka tipa/modela:</label><br/>
-                <input type="text" id="oznakaModela" name="oznakaModela"/><br/><br/>
+                <label htmlFor="oznakaModela">Oznaka tipa/modela:</label>
+                <input type="text" id="oznakaModela" name="oznakaModela"/><br/>
 
-                <label htmlFor="sluzbenaOznaka">Sluzbena oznaka tipa/broj izjave o usaglasenosti:</label><br/>
+                <label htmlFor="sluzbenaOznaka">Sluzbena oznaka tipa/broj izjave o usaglasenosti:</label>
                 <select id="sluzbenaOznaka" name="sluzbenaOznaka">
                     <option value="predlog1">Predlog 1</option>
                     <option value="predlog2">Predlog 2</option>
                     <option value="predlog3">Predlog 3</option>
-                </select><br/><br/>
+                </select><br/>
 
-                <label htmlFor="merniOpseg">Merni opseg:</label><br/>
+                <label htmlFor="merniOpseg">Merni opseg:</label>
                 <select id="merniOpseg" name="merniOpseg">
                     <option value="opcija1">Opcija 1</option>
                     <option value="opcija2">Opcija 2</option>
                     <option value="opcija3">Opcija 3</option>
-                </select><br/><br/>
+                </select><br/>
 
-                <label htmlFor="najmanjiPodeljak">Najmanji podeljak:</label><br/>
+                <label htmlFor="najmanjiPodeljak">Najmanji podeljak:</label>
                 <select id="najmanjiPodeljak" name="najmanjiPodeljak">
                     <option value="opcija1">Opcija 1</option>
                     <option value="opcija2">Opcija 2</option>
                     <option value="opcija3">Opcija 3</option>
-                </select><br/><br/>
+                </select><br/>
 
-                <label htmlFor="klasaTacnosti">Klasa tacnosti:</label><br/>
+                <label htmlFor="klasaTacnosti">Klasa tacnosti:</label>
                 <select id="klasaTacnosti" name="klasaTacnosti">
                     <option value="opcija1">Opcija 1</option>
                     <option value="opcija2">Opcija 2</option>
                     <option value="opcija3">Opcija 3</option>
-                </select><br/><br/>
+                </select><br/>
 
-                <label>Funkcionalna ispravnost:</label><br/>
-                <input type="radio" id="da" name="funkcionalnaIspravnost" value="da"/>
-                <label htmlFor="da">DA</label><br/>
-                <input type="radio" id="ne" name="funkcionalnaIspravnost" value="ne"/>
-                <label htmlFor="ne">NE</label><br/><br/>
+                <label className="container">
+                    <h2>Merna traka je funkcionalno ispravna:</h2>
+                    <input type="radio" id="da" name="funkcionalnaIspravnost" value="da" />
+                    <label htmlFor="da">DA</label>
+                    <input type="radio" id="ne" name="funkcionalnaIspravnost" value="ne" />
+                    <label htmlFor="ne">NE</label>
+                </label>
 
-                <label htmlFor="napomena">Napomena:</label><br/>
-                <textarea id="napomena" name="napomena" rows="4" cols="50"></textarea><br/><br/>
+                <label htmlFor="napomena">Napomena:</label>
+                <textarea id="napomena" name="napomena" rows="4" cols="50"></textarea><br/>
 
-                <label>Odstupanje od nazivne mere:</label><br/>
+                <label>Odstupanje od nazivne mere:</label>
                 <div className="rezultatiContainer">
-                    <label htmlFor="rezultat0" className="rezultatiLabel">0:</label>
+                    <input type="text" value="0" readOnly />
                     <input type="text" id="rezultat0" name="rezultat0" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="rezultat1000" className="rezultatiLabel">1000:</label>
+                    <input type="text" value="1000" readOnly />
                     <input type="text" id="rezultat1000" name="rezultat1000" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="rezultat2000" className="rezultatiLabel">2000:</label>
+                    <input type="text" value="2000" readOnly />
                     <input type="text" id="rezultat2000" name="rezultat2000" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="rezultat3000" className="rezultatiLabel">3000:</label>
+                    <input type="text" value="3000" readOnly />
                     <input type="text" id="rezultat3000" name="rezultat3000" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="rezultat4000" className="rezultatiLabel">4000:</label>
+                    <input type="text" value="4000" readOnly />
                     <input type="text" id="rezultat4000" name="rezultat4000" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="rezultat5000" className="rezultatiLabel">5000:</label>
+                    <input type="text" value="5000" readOnly />
                     <input type="text" id="rezultat5000" name="rezultat5000" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="rezultat6000" className="rezultatiLabel">6000:</label>
+                    <input type="text" value="6000" readOnly />
                     <input type="text" id="rezultat6000" name="rezultat6000" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="rezultat7000" className="rezultatiLabel">7000:</label>
+                    <input type="text" value="7000" readOnly />
                     <input type="text" id="rezultat7000" name="rezultat7000" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="rezultat8000" className="rezultatiLabel">8000:</label>
+                    <input type="text" value="8000" readOnly />
                     <input type="text" id="rezultat8000" name="rezultat8000" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="rezultat9000" className="rezultatiLabel">9000:</label>
+                    <input type="text" value="9000" readOnly />
                     <input type="text" id="rezultat9000" name="rezultat9000" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="rezultat10000" className="rezultatiLabel">10000:</label>
+                    <input type="text" value="10000" readOnly />
                     <input type="text" id="rezultat10000" name="rezultat10000" className="rezultatiInput"/>
                 </div>
 
 
                 <label>Greska podeljka skale:</label><br/>
                 <div className="rezultatiContainer">
-                    <label htmlFor="podeljak660661" className="rezultatiLabel">660-661:</label>
-                    <input type="text" id="rezultat660661" name="rezultat660661" className="rezultatiInput"/>
+                    <input type="text" id="greska1" name="greska1" className="rezultatiInput" placeholder={"Greška podeljka skale"}/>
+                    <input type="text" id="rezultat1" name="rezultat1" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="podeljak661662" className="rezultatiLabel">661-662:</label>
-                    <input type="text" id="rezultat661662" name="rezultat661662" className="rezultatiInput"/>
+                    <input type="text" id="greska2" name="greska2" className="rezultatiInput" placeholder={"Greška podeljka skale"}/>
+                    <input type="text" id="rezultat2" name="rezultat2" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="podeljak13501351" className="rezultatiLabel">1350-1351:</label>
-                    <input type="text" id="rezultat13501351" name="rezultat13501351" className="rezultatiInput"/>
+                    <input type="text" id="greska3" name="greska3" className="rezultatiInput" placeholder={"Greška podeljka skale"}/>
+                    <input type="text" id="rezultat3" name="rezultat3" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="podeljak13511352" className="rezultatiLabel">1351-1352:</label>
-                    <input type="text" id="rezultat13511352" name="rezultat13511352" className="rezultatiInput"/>
+                    <input type="text" id="greska4" name="greska4" className="rezultatiInput" placeholder={"Greška podeljka skale"}/>
+                    <input type="text" id="rezultat4" name="rezultat4" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="podeljak58005801" className="rezultatiLabel">5800-5801:</label>
-                    <input type="text" id="rezultat58005801" name="rezultat58005801" className="rezultatiInput"/>
+                    <input type="text" id="greska5" name="greska5" className="rezultatiInput" placeholder={"Greška podeljka skale"}/>
+                    <input type="text" id="rezultat5" name="rezultat5" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="podeljak58015802" className="rezultatiLabel">5801-5802:</label>
-                    <input type="text" id="rezultat58015802" name="rezultat58015802" className="rezultatiInput"/>
+                    <input type="text" id="greska6" name="greska6" className="rezultatiInput" placeholder={"Greška podeljka skale"}/>
+                    <input type="text" id="rezultat6" name="rezultat6" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                    <label htmlFor="podeljak86508651" className="rezultatiLabel">8650-8651:</label>
-                    <input type="text" id="rezultat86508651" name="rezultat86508651" className="rezultatiInput"/>
+                    <input type="text" id="greska7" name="greska7" className="rezultatiInput" placeholder={"Greška podeljka skale"}/>
+                    <input type="text" id="rezultat7" name="rezultat7" className="rezultatiInput"/>
                 </div>
                 <div className="rezultatiContainer">
-                <label htmlFor="podeljak86518652" className="rezultatiLabel">8651-8652:</label>
-                    <input type="text" id="rezultat86518652" name="rezultat86518652" className="rezultatiInput"/>
+                    <input type="text" id="greska8" name="greska8" className="rezultatiInput" placeholder={"Greška podeljka skale"}/>
+                    <input type="text" id="rezultat8" name="rezultat8" className="rezultatiInput"/>
                 </div>
 
 
-                <label htmlFor="merniLenjir">Serijski broj Mernog lenjira:</label><br/>
+                <label htmlFor="merniLenjir">Serijski broj Mernog lenjira:</label>
                 <select id="merniLenjir" name="merniLenjir">
                     <option value="opcija1">Opcija 1</option>
                     <option value="opcija2">Opcija 2</option>
                     <option value="opcija3">Opcija 3</option>
-                </select><br/><br/>
+                </select><br/>
 
-                <label htmlFor="mernaLupa">Serijski broj Merne lupe:</label><br/>
+                <label htmlFor="mernaLupa">Serijski broj Merne lupe:</label>
                 <select id="mernaLupa" name="mernaLupa">
                     <option value="opcija1">Opcija 1</option>
                     <option value="opcija2">Opcija 2</option>
                     <option value="opcija3">Opcija 3</option>
-                </select><br/><br/>
+                </select><br/>
 
-                <label htmlFor="skinutiZigovi1">Skinuti Zigovi:</label><br/>
-                <input type="text" id="skinutiZigovi1" name="skinutiZigovi1"/><br/>
-                <input type="text" id="skinutiZigovi2" name="skinutiZigovi2"/><br/><br/>
+                <label htmlFor="skinutiZigovi1">Skinuti Zigovi:</label>
+                <input type="text" id="skinutiZigovi1" name="skinutiZigovi1"/>
+                <input type="text" id="skinutiZigovi2" name="skinutiZigovi2"/><br/>
 
-                <label htmlFor="stavljeniZigovi1">Stavljeni Zigovi:</label><br/>
-                <input type="text" id="stavljeniZigovi1" name="stavljeniZigovi1"/><br/>
-                <input type="text" id="stavljeniZigovi2" name="stavljeniZigovi2"/><br/><br/>
+                <label htmlFor="stavljeniZigovi1">Stavljeni Zigovi:</label>
+                <input type="text" id="stavljeniZigovi1" name="stavljeniZigovi1"/>
+                <input type="text" id="stavljeniZigovi2" name="stavljeniZigovi2"/><br/>
 
-                <label htmlFor="ispunjava">Merilo ispunjava propisane zahteve:</label>
-                <input type="radio" id="ispunjava" name="ispunjava" value="ispunjava"/>
-                <label htmlFor="ispunjava">DA</label><br/>
-                <input type="radio" id="neIspunjava" name="ispunjava" value="neIspunjava"/>
-                <label htmlFor="neIspunjava">NE</label><br/><br/>
+                <label className="container">
+                    <h2>Merna traka ispunjava propisane zahteve:</h2>
+                    <input type="radio" id="ispunjava" name="ispunjava" value="ispunjava" />
+                    <label htmlFor="ispunjava">DA</label>
+                    <input type="radio" id="neIspunjava" name="ispunjava" value="neIspunjava" />
+                    <label htmlFor="neIspunjava">NE</label>
+                </label>
 
-                <label htmlFor="komentar">Komentar:</label><br/>
-                <textarea id="komentar" name="komentar" rows="4" cols="50"></textarea><br/><br/>
+                <label htmlFor="komentar">Komentar:</label>
+                <textarea id="komentar" name="komentar" rows="4" cols="50"></textarea><br/>
 
-                <label htmlFor="datum">Datum:</label><br/>
-                <input type="date" id="datum" name="datum"/><br/><br/>
+                <label htmlFor="datum">Datum:</label>
+                <input type="date" id="datum" name="datum"/><br/>
 
                 <input type="submit" value="Potvrdi"/>
             </form>
