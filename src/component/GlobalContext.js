f89f0cc5c6257537c2a569e0df34f0d92994356c
Jovan Stankovic <j.stankovic001@gmail.com>
2024-06-18 17:13:22 +0200

    Update

--- src/component/GlobalContext.js
+++ src/component/GlobalContext.js
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 const GlobalContext = createContext();
 
 const GlobalProvider = ({ children }) => {
-    const [globalVariable, setGlobalVariable] = useState('http://localhost:8080');
+    const [globalVariable, setGlobalVariable] = useState('http://192.168.1.27:8080');
 
     return (
         <GlobalContext.Provider value={{ globalVariable, setGlobalVariable }}>
